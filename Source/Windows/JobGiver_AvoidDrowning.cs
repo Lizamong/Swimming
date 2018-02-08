0dad3b86108f5eb9c4c7c3847fa3b7176ff64777
Lizamong <34539466+Lizamong@users.noreply.github.com>
2018-02-08 19:46:58 +0900

    lol

--- Source/Windows/JobGiver_AvoidDrowning.cs
+++ Source/Windows/JobGiver_AvoidDrowning.cs
@@ -13,19 +13,21 @@ namespace Swimming
 
         protected override Job TryGiveJob(Pawn pawn)
         {
-            Log.Message("1");
-            bool isDrowning = pawn.health.hediffSet.HasHediff(HediffDef.Named("LZG_Drowning"));
-            bool canDoJobNow = pawn.CurJob.def != JobDefOf.Goto &&
-                           pawn.CurJob.def !=  DefDatabase<JobDef>.GetNamed("LZG_FindTreasure");
-            if (isDrowning && canDoJobNow)
+            bool isDrowning = pawn?.health?.hediffSet?.HasHediff(HediffDef.Named("LZG_Drowning")) ?? false;
+            bool canDoJobNow = pawn?.CurJob?.def != JobDefOf.Goto &&
+               pawn?.CurJob?.def !=  DefDatabase<JobDef>.GetNamed("LZG_FindTreasure");
+           if (isDrowning && canDoJobNow)
             {
-                IntVec3 White = FindOrangeLand(pawn);
-                if (White.IsValid)
-                {
-                    Job Escape_from_eternal_death = new Job(JobDefOf.Goto, White);
-                    return Escape_from_eternal_death;
-                }
-      
+               if (Find.TickManager.TicksGame % 60 == 0)
+               {
+                    IntVec3 White = FindOrangeLand(pawn);
+                    if (White.IsValid)
+                    {
+                        Job Escape_from_eternal_death = new Job(JobDefOf.Goto, White);
+                        Escape_from_eternal_death.locomotionUrgency = LocomotionUrgency.Sprint;
+                        return Escape_from_eternal_death;
+                    }
+               }
             }
             return null;
         }
@@ -40,19 +42,21 @@ namespace Swimming
                 {
                     if (intVec is IntVec3 vec && vec.IsValid &&
                         vec.GetTerrain(pawn.MapHeld) is TerrainDef def &&
-                      (def == TerrainDefOf.WaterDeep ||
-                        def == TerrainDefOf.WaterMovingDeep ||
-                        def == TerrainDefOf.WaterOceanDeep ))
+                      (def != TerrainDefOf.WaterDeep &&
+                        def != TerrainDefOf.WaterMovingDeep &&
+                        def != TerrainDefOf.WaterOceanDeep ))
                     {
 
                         if (GenSight.LineOfSight(position, intVec, pawn.Map, false, null, 0, 0))
                         {
+                            Log.Message(intVec.ToString());
                             return intVec;
                         }
                     }
 
                 }
             }
+            Log.Message(IntVec3.Invalid.ToString());
             return IntVec3.Invalid;
         }
 
