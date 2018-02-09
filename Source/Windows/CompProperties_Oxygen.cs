cf1e43913f6018fd17eca8755b1b8eb638b8dbb7
Lizamong <34539466+Lizamong@users.noreply.github.com>
2018-02-09 20:02:06 +0900

    Oxygen Tank

---
+++ Source/Windows/CompProperties_Oxygen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace Swimming
+{
+    class CompProperties_Oxygen : CompProperties
+    {
+        public int ticksToGain = 120;
+        public int ticksToLose = 150;
+        public int airMax = 80;
+        public CompProperties_Oxygen()
+        {
+
+            this.compClass = typeof(OxygenTank);
+        }
+    }
+}
---
+++ Source/Windows/OxygenTank.cs
@@ -0,0 +1,67 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace Swimming
+{
+    class OxygenTank : ThingComp
+    {
+        public override void CompTick()
+        {
+            base.CompTick();
+            IntVec3 intVec = this.parent.Position;
+            if (intVec is IntVec3 vec && vec.IsValid &&
+                vec.GetTerrain(this.parent.MapHeld) is TerrainDef def &&
+              (def != TerrainDefOf.WaterDeep &&
+                def != TerrainDefOf.WaterMovingDeep &&
+                def != TerrainDefOf.WaterOceanDeep))
+            {
+                LoseAir();
+
+            }
+            else
+            {
+                GetAir();
+            }
+                }
+        public void GetAir()
+        {
+            if (Find.TickManager.TicksGame % Props.ticksToGain == 0) ;
+            {
+                Air++;
+            }
+            if (Find.TickManager.TicksGame % Props.ticksToLose == 0) ;
+            {
+                Air--;
+            }
+        }
+        public void LoseAir()
+        {
+
+        }
+        public CompProperties_Oxygen Props
+        {
+            get
+            {
+                return (CompProperties_Oxygen)this.props;
+            }
+        }
+        int air = 0;
+        int airMax = 80;
+        public int Air
+        {
+            get
+            {
+                return air;
+            }
+            set
+            {
+                air = Mathf.Clamp(value, 0, Props.airMax);
+            }
+        }
+    }
+}
